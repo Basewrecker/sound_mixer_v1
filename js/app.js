0b97bcc884434099835c1385d7fddcc81818c4eb
Basewrecker <siddhunrs@gmail.com>
2025-12-29 21:52:47 +0530

    added sound data obj and init

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+import {sounds, defaultPresets} from "./soundData.js";
+
+class AmbientMixer {
+    constructor() {
+        console.log("Initializing state...");
+        this.soundManager = null;
+        this.ui = null;
+        this.presetManager = null;
+        this.timer = null;
+        this.currentSoundState = {};
+        this.isInitialized = false;
+    }
+    
+    init() {
+        try {
+            console.log("Initializing app...")
+            this.isInitialized = true;
+        } catch (error) {
+            console.error("failed to initialize app", error);
+        }
+    }
+}
+
+// Initialize app for dom
+
+document.addEventListener('DOMContentLoaded', () => {
+    const app = new AmbientMixer();
+    app.init();
+})
